122eefc798b2c158c969d168eadb218403fcf44b
erikelliott-ynp <erikelliott92@gmail.com>
2020-01-16 10:57:51 -0800

    add alert button

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,18 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    
     var body: some View {
         VStack {
             Text("Welcome to my first App!")
                 .font(.callout)
                 .fontWeight(.semibold)
                 .foregroundColor(Color.orange)
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Button(action: {
+                print("Button pressed!")
+                self.alertIsVisible = true
+            }) {
                 Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
                     .fontWeight(.semibold)
                     .foregroundColor(Color.purple)
             }
+            .alert(isPresented: $alertIsVisible) { () -> Alert in
+                return Alert(title: Text("Hello There (;"), message: Text("This is my first Pop-up"), dismissButton: .default(Text("Jawesome!")))
+                
+            }
         }
+        
     }
 }
 
